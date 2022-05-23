90bbfeee3f1c0cef7e8d1304638dd81049e14bec
Kirill <glukirill@rambler.ru>
2022-05-23 13:22:39 +0300

    Fiest Commit

---
+++ src/main/kotlin/com/example/prelimbclone/PrelimBCloneApplication.kt
@@ -0,0 +1,12 @@
+package com.example.prelimbclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrelimBCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrelimBCloneApplication>(*args)
+    println("Hello World")
+}
---
+++ src/test/kotlin/com/example/prelimbclone/PrelimBCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.prelimbclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrelimBCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
