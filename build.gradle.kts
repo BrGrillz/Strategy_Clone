3d904c3916c8e9d985583eb613518ec1eb497473
Kirill <glukirill@rambler.ru>
2022-06-24 17:57:09 +0300

    heroku deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,3 +46,9 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.example.prelimbclone.PrelimBCloneApplication.kt"
+    }
+}
