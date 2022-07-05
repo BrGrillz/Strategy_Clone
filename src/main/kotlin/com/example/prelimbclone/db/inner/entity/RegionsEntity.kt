fca437c7e6cc4901afbfd0b1ec7db2cdb55ba281
Kirill <glukirill@rambler.ru>
2022-07-05 13:41:35 +0300

    Упакованная версия для тестирования aka говно

--- src/main/kotlin/com/example/prelimbclone/db/inner/entity/RegionsEntity.kt
+++ src/main/kotlin/com/example/prelimbclone/db/inner/entity/RegionsEntity.kt
@@ -1,15 +1,15 @@
 package com.example.prelimbclone.db.inner.entity
 
-import javax.persistence.*
 
-@Entity
-@Table(name = "regions")
-class RegionsEntity (
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Int = 0,
-    var regionCode: String = "",
-    @Column(nullable = true)
-    var regionCity: String = "",
-    var result: Int = 0,
-)
+
+//@Entity
+//@Table(name = "regions")
+//class RegionsEntity (
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    var id: Int = 0,
+//    var regionCode: String = "",
+//    @Column(nullable = true)
+//    var regionCity: String = "",
+//    var result: Int = 0,
+//)
